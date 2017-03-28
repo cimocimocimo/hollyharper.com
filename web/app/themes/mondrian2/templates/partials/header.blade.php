be32e63a0f3ce6a7b899a11038de1de618062a20
Aaron Cimolini <aaron@cimolini.com>
2017-03-27 22:28:14 -0700

    Adds the SVG logos.

--- web/app/themes/mondrian2/templates/partials/header.blade.php
+++ web/app/themes/mondrian2/templates/partials/header.blade.php
@@ -1,10 +1,22 @@
+<?php echo file_get_contents( get_template_directory() . '/assets/svgs/inline/filters.svg'); ?>
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+  <div class="header__nav-bg"></div>
+  <div class="header__container">
+    <div class="header__branding">
+      <a class="site-logo" href="{{ home_url('/') }}">
+
+        <?php echo file_get_contents( get_template_directory() . '/assets/svgs/inline/Holly-Harper-logo-full.svg'); ?>
+        <?php echo file_get_contents( get_template_directory() . '/assets/svgs/inline/Holly-Harper-logo-condensed.svg'); ?>
+        <?php echo file_get_contents( get_template_directory() . '/assets/svgs/inline/Holly-Harper-logo-abbreviated.svg'); ?>
+
+      </a>
+    </div>
+    <div class="header__nav">
+      <nav class="nav-primary">
+        @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        @endif
+      </nav>
+    </div>
   </div>
 </header>
