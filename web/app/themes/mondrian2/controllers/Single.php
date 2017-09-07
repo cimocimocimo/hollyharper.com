9ebf32d1cc7c13a2545b91b7131b5a50ce9788ad
Brett Goss <goss.brett@gmail.com>
2017-09-07 13:04:26 -0700

    Move post thumbnails and categories functions to controller

--- web/app/themes/mondrian2/controllers/Single.php
+++ web/app/themes/mondrian2/controllers/Single.php
@@ -6,11 +6,11 @@ use Sober\Controller\Controller;
 
 class Single extends Controller
 {
-    /**
-     * Return foo from Advanced Custom Fields
-     *
-     * @return array
-     */
+     /**
+        * Return foo from Advanced Custom Fields
+        *
+        * @return array
+        */
 
     public function old_single_post_data()
     {
@@ -18,7 +18,22 @@ class Single extends Controller
             'slide_img_size' => 'slideshow-large',
             'pager_img_size' => 'slideshow-thumb'
         ];
+
+        // check for a post thumbnail, all listings SHOULD have one. 
+        if ( has_post_thumbnail() ) {
+            $data['thumb_id'] = get_post_thumbnail_id();
+        }
+
         return $data;
+    }
+
+    public static function old_single_post_in_loop_data()
+    {
+        $loop_data = [
+            'categories' => get_the_category()
+        ];
+
+        return $loop_data;
     }
 
 }
--- web/app/themes/mondrian2/templates/single.blade.php
+++ web/app/themes/mondrian2/templates/single.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.base')
 
 @section('content')
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  @if($old_single_post_data)
+    {!! var_dump($old_single_post_data) !!}
+  @endif
+
   @while(have_posts()) @php(the_post())
     @include('partials/content-single-'.get_post_type())
-    @if($old_single_post_data)
-      {!! var_dump($old_single_post_data) !!}
+
+    @if( Single::old_single_post_in_loop_data() )
+      {!! var_dump(Single::old_single_post_in_loop_data()) !!}
     @endif
+
   @endwhile
 @endsection
