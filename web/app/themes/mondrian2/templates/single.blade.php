a9713d1d33c5a7f885775400b57453ec44f21c38
Brett Goss <goss.brett@gmail.com>
2017-09-07 12:27:39 -0700

    Use old post data controller

--- web/app/themes/mondrian2/templates/single.blade.php
+++ web/app/themes/mondrian2/templates/single.blade.php
@@ -3,6 +3,8 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     @include('partials/content-single-'.get_post_type())
+    @if($old_single_post_data)
+      {!! var_dump($old_single_post_data) !!}
+    @endif
   @endwhile
 @endsection
-
