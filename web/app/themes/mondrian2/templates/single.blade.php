8651bc63db327ec402c58eb6f616c39fdc032a45
Brett Goss <goss.brett@gmail.com>
2017-09-18 10:39:03 -0700

    Add check_if_blog to view

--- web/app/themes/mondrian2/templates/single.blade.php
+++ web/app/themes/mondrian2/templates/single.blade.php
@@ -17,5 +17,11 @@
       {!! var_dump(Single::old_single_post_in_loop_data()) !!}
     @endif
 
+    @if( Single::check_if_blog() )
+      <h1>This is a blog</h1>
+    @else
+      <h1>This is not a blog</h1>
+    @endif
+
   @endwhile
 @endsection
